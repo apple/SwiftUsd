90c1aadd6c0d6797afa98002176894601e9108bc
Maddy Adams <maddy_adams@apple.com>
2025-08-08 17:37:18 -0700

    Update source file copyright notices

--- source/Wrappers/TfDiagnosticsWrapper.swift
+++ source/Wrappers/TfDiagnosticsWrapper.swift
@@ -1,21 +1,22 @@
-// ===-------------------------------------------------------------------===//
+//===----------------------------------------------------------------------===//
 // This source file is part of github.com/apple/SwiftUsd
 //
-// Copyright © 2025 Apple Inc. and the SwiftUsd authors. All Rights Reserved. 
+// Copyright © 2025 Apple Inc. and the SwiftUsd project authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at: 
-// https://www.apache.org/licenses/LICENSE-2.0
+// You may obtain a copy of the License at
+//
+//  https://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.     
-// 
+// limitations under the License.
+//
 // SPDX-License-Identifier: Apache-2.0
-// ===-------------------------------------------------------------------===//
+//===----------------------------------------------------------------------===//
 
 // Due to pxr::TfEnum::TfEnum(_:_:) using enable_if, Swift can't import
 // that constructor. Due to https://github.com/swiftlang/swift/issues/83081 (Templated C++ function incorrectly imported as returning Void in Swift),
